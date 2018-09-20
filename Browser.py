1a0f012ed59d997c1e1418364c4206d8ca766391
Nikita Mihaylov <n.mihailov@netology-group.ru>
2018-09-20 22:50:32 +0300

    y

--- Browser.py
+++ Browser.py
@@ -1,12 +1,12 @@
 
 # coding: utf-8
 
-# In[5]:
+# In[6]:
 
 
 import os
 try:
-    os.startfile(r'C:\Program Files (x86)\Google\Chrome\Application\chome.exe')
+    os.startfile(r'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe')
 except FileNotFoundError:
     path = input('Ваш браузер не хранится в папке "по умолчанию", введите пожалуйста полный путь в формате C:\..\chrome.exe: ')
     os.startfile(path)
